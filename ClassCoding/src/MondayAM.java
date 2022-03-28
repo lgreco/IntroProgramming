135b0b66f4294be758baaeff9eefcb10564aed9b
lirakliotis <lgreco@gmail.com>
2022-03-28 09:12:43 -0500

    largest letter

--- ClassCoding/src/MondayAM.java
+++ ClassCoding/src/MondayAM.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MondayAM {
 
     /*
@@ -11,6 +13,61 @@ public class MondayAM {
      */
 
     public static void main(String[] args) {
+        largestOfTwoLetters();
+    }
 
+    /*
+    PSEUDOCODE
+    largest <-- given by user
+    next_largest <-- also given by user
+    input <-- next item given by user
+    while (input is NOT a number) {
+      ensure that largest > next_largest
+      find a way to replace largest or next_largest with input received just now, if that letter
+        is greater than both largest and next_largest.
+        - how to tell if input is greater than largest and next_largest?
+          if input > max(largest, next_largest):
+             now we know that one of these two numbers needs to be replaced
+             next_largest <-- input
+    }
+    // report two largest letters
+
+
+    TOOLS WE NEED:
+    Scanner
+
+     */
+
+    public static boolean isNumber(String string) {
+        char c = string.charAt(0);
+        boolean result = c >= '0' && c <= '9';
+        System.out.println(result);
+        return result;
+    }
+
+    public static void largestOfTwoLetters() {
+        Scanner s = new Scanner(System.in);
+        String largest, next_largest, inputLetter;
+        char large='a', next_large='a', input;
+        // obtain initial letters expecting that the first two inputs are legit letters
+        largest = s.next();
+        next_largest = s.next();
+        inputLetter = s.next();
+        while (!isNumber(inputLetter)) {  // Telling if single letter string is not a number?
+            // Convert inputs to chars
+            large = largest.charAt(0);
+            next_large = next_largest.charAt(0);
+            // Ensure proper order between large and next_large
+            if (next_large > large) {
+                char temp = large;
+                large = next_large;
+                next_large = temp;
+            }
+            input = inputLetter.charAt(0);
+            if (input > Math.max(large, next_large))
+                next_large = input;
+            inputLetter = s.next();
+        }
+        System.out.println((char)large+(char)next_large);
     }
 }
