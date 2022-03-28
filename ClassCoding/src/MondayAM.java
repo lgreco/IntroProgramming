2d938fdf2bbef0fc478b137770b342a68f47fddf
lirakliotis <lgreco@gmail.com>
2022-03-28 08:30:16 -0500

    largest letter

---
+++ ClassCoding/src/MondayAM.java
@@ -0,0 +1,16 @@
+public class MondayAM {
+
+    /*
+    Write a program that asks users to type letters (one letter at a
+    time) and ends when the user types a number. When the user types
+    a number, the program reports the two "largest" letters. Size is
+    defined by the position of the letter in the alphabet. We assume
+    that only lower case letters are entered. Smallest letter is 'a'
+    and largest letter  is  'z'.  Your code  must have  at least two
+    methods (including main).
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
