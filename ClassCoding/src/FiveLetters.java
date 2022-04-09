13437211ba4a97d125b0bf99f4820392fd27cc07
Leo Irakliotis <lgreco@gmail.com>
2022-04-09 10:27:26 -0500

    SP22 words.txt

--- ClassCoding/src/FiveLetters.java
+++ ClassCoding/src/FiveLetters.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -98,8 +100,17 @@ public class FiveLetters {
         return found;
     }
 
-    public static void main(String[] args) throws Exception {
-        String[] fiveLetters = readWords(LINK);
-        System.out.println(isValidBinary("hello", fiveLetters));
+    public static boolean quickCheck(String word) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("words.txt"));
+        while (sc.hasNextLine())
+            if (sc.nextLine().toLowerCase().equals(word.toLowerCase()))
+                return true;
+        return false;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+       //String[] fiveLetters = readWords(LINK);
+       // System.out.println(isValidBinary("hello", fiveLetters));
+        System.out.println(quickCheck("hello"));
     }
 }
